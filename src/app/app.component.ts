d6fbb6c1e7f7553b21dc969f9d19fd1d2d302f50
Bin291 <phuongbinh200429@gmail.com>
2025-04-07 00:04:01 +0700

    feat: app sort 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SortLabComponent} from './components/sort-lab/sort-lab.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SortLabComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
